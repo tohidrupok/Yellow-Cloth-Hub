61ddb7634a7d274785fada1b2b3283ba6fbdf85e
tohidrupok <pcgamertohid@gmail.com>
2023-09-03 12:53:22 +0600

    add media

--- django_mart/settings.py
+++ django_mart/settings.py
@@ -119,21 +119,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-# STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'static'
-# MEDIA_URL = 'media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
-
-# STATICFILES_DIRS = [
-#     BASE_DIR/'static',
-# ]
-
 
 
 STATIC_URL = '/static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 if not DEBUG:STATIC_ROOT = ''
 
 STATICFILES_DIRS = [
